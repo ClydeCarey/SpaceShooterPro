1ff093b5c8fee197dea66b83970d18196c1e1b01
ClydeCarey <73868789+ClydeCarey@users.noreply.github.com>
2020-11-10 07:35:42 -1000

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float _enemyVSpeed = 4.0f;
+    private float _speed = 4.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +16,14 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         //move down 4m/s
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
         //respawn at the top if it hits bottom
-
+        if(transform.position.y <-5.0f)
+        {
+            float randomX = Random.Range(-8.0f, 8.0f);
+            transform.position = new Vector3(randomX, 7, 0);
+        }
         //respan at top with noe wrandom position
     }
 }
